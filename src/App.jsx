0bd30f4208e7ad094dead1fb9f8f0c49472d033f
AAbAlQahtani <asmaalqhtani123@gmail.com>
2025-05-20 14:00:14 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Nav from './components/Nav'
-import Cards from './components/cards'
+import Cards from './components/Cards'
 import Footer from './components/footer'
 import Hero from './components/hero'
 
